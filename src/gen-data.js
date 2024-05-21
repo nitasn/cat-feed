4a0ac4339ad7b9ffd0b60da6728393420fa85780
Nitsan BenHanoch <nitsanbh@gmail.com>
2024-05-21 22:35:52 +0300

    forgot to commit file

---
+++ src/gen-data.js
@@ -0,0 +1,72 @@
+// @ts-check
+
+/** @type {Name[]} */
+const names = ["imma", "nitsan", "tal", "ronnie", "shahar"];
+
+/**
+ * Random sampling with repetitions.
+ * @template T
+ * @param {T[]} array
+ * @param {number} k
+ * @returns {T[]}
+ */
+function randomSample(array, k) {
+  const result = [];
+  for (let i = 0; i < k; i++) {
+    result.push(randomChoice(array));
+  }
+  return result;
+}
+
+/**
+ * @template T
+ * @param {T[]} array
+ * @returns {T}
+ */
+function randomChoice(array) {
+  const index = Math.floor(Math.random() * array.length);
+  return array[index];
+}
+
+/**
+ * @template T
+ * @param {T[]} array
+ * @returns {T[]}
+ */
+function noDups(array) {
+  return Array.from(new Set(array));
+}
+
+/**
+ * @template T
+ * @param {T[]} A
+ * @param {T[]} B
+ * @returns {T[]}
+ */
+function arrayDifference(A, B) {
+  return A.filter((a) => !B.includes(a));
+}
+
+/**
+ * @returns {{ positive: Name[], negative: Name[] }}
+ */
+function randomPosNeg() {
+  const maxPos = randomChoice([0, 1, 2, 3, 4, 5]);
+  const positive = noDups(randomSample(names, maxPos));
+
+  const remainingNames = arrayDifference(names, positive);
+
+  const maxNeg = randomChoice([0, 1, 2]);
+  const negative = noDups(randomSample(remainingNames, maxNeg));
+
+  return { positive, negative };
+}
+
+/**
+ * @returns {WeeklyData}
+ */
+export function randomWeeklyData() {
+  const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+  const daysEntries = days.map((day) => [day, { morning: randomPosNeg(), evening: randomPosNeg() }]);
+  return Object.fromEntries(daysEntries);
+}
