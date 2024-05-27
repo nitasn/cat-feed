1c8cab70e604c922096b43237c1c4f8749673b99
Nitsan BenHanoch <nitsanbh@gmail.com>
2024-05-27 07:09:45 +0300

    added auth (signing the time)

--- src/server-bridge.ts
+++ src/server-bridge.ts
@@ -1,19 +1,27 @@
+import JWT from "expo-jwt";
+import { SupportedAlgorithms } from "expo-jwt/dist/types/algorithms";
 import { emptyWeeklyData } from "./gen-data";
 import { type MealPath, type Name, type PosNeg, type WeekData } from "./types";
 
 const SERVER_URL = "http://192.168.1.120:3000";
-
 // const SERVER_URL = "https://cat-feed-server.vercel.app";
 
+function authorizationHeader() {
+  const payload = { iat: Date.now() };
+  const secret = process.env.EXPO_PUBLIC_JWT_SECRET as string;
+  const token = JWT.encode(payload, secret, { algorithm: SupportedAlgorithms.HS512 });
+  return { Authorization: `Bearer ${token}` };
+}
+
 async function fetchGET(path: string) {
-  const res = await fetch(SERVER_URL + path);
+  const res = await fetch(SERVER_URL + path, { headers: authorizationHeader() });
   return await res.json();
 }
 
 async function fetchPOST(path: string, body: Object) {
   const res = await fetch(SERVER_URL + path, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: { ...authorizationHeader(), "Content-Type": "application/json" },
     body: JSON.stringify(body),
   });
   return await res.json();
